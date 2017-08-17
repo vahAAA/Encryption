401d08e8f0dab6aef9b8e9c468423528ceb6ebb8
Ivan <evanvaha@gmail.com>
2017-08-17 11:11:18 +0300

    added simultaneous enc and dec for both AES and RSA

--- src/Encryption.java
+++ src/Encryption.java
@@ -7,9 +7,12 @@ import java.security.spec.InvalidKeySpecException;
 public class Encryption {
 
     //HARDCODED, should be changed
-    public static String ENCRYPT_FILENAME = System.getProperty("user.home") + "\\Desktop\\encrypted.txt";
+
     public static String MESSAGE_FILENAME = System.getProperty("user.home") + "\\Desktop\\message.txt";
-    public static String DECRYPT_FILENAME = System.getProperty("user.home") + "\\Desktop\\decrypted.txt";
+    public static String ENCRYPT_FILENAME_AES = System.getProperty("user.home") + "\\Desktop\\encryptedAES.txt";
+    public static String ENCRYPT_FILENAME_RSA = System.getProperty("user.home") + "\\Desktop\\encryptedRSA.txt";
+    public static String DECRYPT_FILENAME_AES = System.getProperty("user.home") + "\\Desktop\\decryptedAES.txt";
+    public static String DECRYPT_FILENAME_RSA = System.getProperty("user.home") + "\\Desktop\\decryptedRSA.txt";
     public static String key = "Bar12345Bar12345";
     public static String initVector = "RandomInitVector";
 
@@ -18,17 +21,20 @@ public class Encryption {
      *
      * for now you should change methods, in case you want to use AES instead of RSA (to be changed)
      */
-    private static void readFromFile(String key, String initVector, boolean ifEncrypt, String filename) {
+    private static void readFromFile(String key, String initVector, boolean ifEncrypt, String filename, boolean ifRSA) {
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream(filename), StandardCharsets.UTF_8))) {
             String line = reader.readLine();
             if (ifEncrypt) {
-                //writeToFile(AES.encryptAES(key, initVector, line), ENCRYPT_FILENAME);
-                writeToFile(RSA.encryptRSA(line), ENCRYPT_FILENAME);
+                writeToFile(AES.encryptAES(key, initVector, line), ENCRYPT_FILENAME_AES);
+                writeToFile(RSA.encryptRSA(line), ENCRYPT_FILENAME_RSA);
             } else {
-                //writeToFile(AES.decryptAES(key, initVector, line), DECRYPT_FILENAME);
-                writeToFile(RSA.decryptRSA(line), DECRYPT_FILENAME);
+                if(ifRSA)
+                    writeToFile(RSA.decryptRSA(line), DECRYPT_FILENAME_RSA);
+                else
+                    writeToFile(AES.decryptAES(key, initVector, line), DECRYPT_FILENAME_AES);
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -51,10 +57,13 @@ public class Encryption {
         RSA.generateKey();
 
         //Read message from file and encrypt with AES
-        readFromFile(key, initVector, true, MESSAGE_FILENAME);
+        readFromFile(key, initVector, true, MESSAGE_FILENAME,true);
 
         //Read encrypted message from file and decrypt with AES
-        readFromFile(key, initVector, false, ENCRYPT_FILENAME);
+        readFromFile(key, initVector, false, ENCRYPT_FILENAME_AES,false);
+
+        //Read encrypted message from file and decrypt with AES
+        readFromFile(key, initVector, false, ENCRYPT_FILENAME_RSA,true);
     }
 }
 
